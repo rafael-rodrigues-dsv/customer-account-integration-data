97d631f8acc460a90980f48056329a4c7ee93b34
Rafael Rodrigues <rafael.rodrigues.dsv@gmail.com>
2024-03-09 18:18:18 -0300

    Refatoração do serviço

--- src/main/java/br/com/customeraccountintegration/data/MainApplication.java
+++ src/main/java/br/com/customeraccountintegration/data/MainApplication.java
@@ -1,76 +1,104 @@
 package br.com.customeraccountintegration.data;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
 import java.text.SimpleDateFormat;
 import java.util.Base64;
 import java.util.Date;
 import java.util.Random;
 
 public class MainApplication {
+  public static void main(String[] args) {
+    DataGenerator dataGenerator = new DataGenerator();
+    dataGenerator.generateAndWriteTestData();
+  }
+}
+
+class DataGenerator {
   private static final Base64.Decoder BASE64_DECODER = Base64.getDecoder();
 
-  public static void main(String[] args) {
+  public void generateAndWriteTestData() {
     String fileName = generateOutputFileName();
     int numberOfItems = 1;
 
-    try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
-      for (int i = 0; i < numberOfItems; i++) {
-        String line = generateRandomLine();
-        writer.write(line);
-        writer.newLine();
-      }
-      System.out.println("Test data generated successfully!");
+    try {
+      createOutputDirectory(); // Cria o diretório de saída
 
-      // Copie o arquivo gerado para o diretório resources
-      copyFileToResources(fileName);
+      try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+        for (int i = 0; i < numberOfItems; i++) {
+          String line = generateRandomLine();
+          writer.write(line);
+          writer.newLine();
+        }
+        System.out.println("Test data generated successfully!");
+
+        // Copie o arquivo gerado para o diretório resources
+        FileCopier.copyFileToResources(fileName);
+      }
     } catch (IOException e) {
       System.err.println("Error writing to file: " + e.getMessage());
     }
   }
 
-  private static String generateOutputFileName() {
+  private String generateOutputFileName() {
     SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
     String dateFormatted = dateFormat.format(new Date());
-    return "files/output/output_data_" + dateFormatted + ".txt";
+    return "src/main/resources/files/output/output_data_" + dateFormatted + ".txt";
   }
 
-  private static String generateRandomLine() {
+  private void createOutputDirectory() throws IOException {
+    Path outputDir = Paths.get("src/main/resources/files/output/");
+    Files.createDirectories(outputDir);
+  }
+
+  private String generateRandomLine() {
     Random random = new Random();
     long documentNumber = 10000000000L + (long) (random.nextDouble() * 90000000000L);
     String email = "customer" + (random.nextInt(1000) + 1) + "@email.com";
-    String decodedString = readBase64FromFile("files/input/input_base64.txt");
+    String decodedString = FileReaderUtil.readBase64FromFile("files/input/input_base64.txt");
     return documentNumber + ";" + email + ";" + decodedString + "|";
   }
+}
 
-  private static String readBase64FromFile(String filePath) {
-    try (BufferedReader reader = new BufferedReader(new InputStreamReader(
-        MainApplication.class.getClassLoader().getResourceAsStream(filePath), StandardCharsets.UTF_8))) {
-      StringBuilder result = new StringBuilder();
-      String line;
+class FileReaderUtil {
+  public static String readBase64FromFile(String filePath) {
+    try (InputStream inputStream = FileReaderUtil.class.getClassLoader().getResourceAsStream(filePath)) {
+      if (inputStream != null) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+          StringBuilder result = new StringBuilder();
+          String line;
 
-      while ((line = reader.readLine()) != null) {
-        result.append(line);
+          while ((line = reader.readLine()) != null) {
+            result.append(line);
+          }
+
+          return result.toString();
+        }
+      } else {
+        // Tenta ler como caminho absoluto
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+          StringBuilder result = new StringBuilder();
+          String line;
+
+          while ((line = reader.readLine()) != null) {
+            result.append(line);
+          }
+
+          return result.toString();
+        }
       }
-
-      return result.toString();
     } catch (IOException e) {
       e.printStackTrace(); // Handle the exception according to your needs
-      return ""; // Return an empty string or handle the error as needed
+      return "";
     }
   }
+}
 
-  private static void copyFileToResources(String sourceFilePath) {
+class FileCopier {
+  public static void copyFileToResources(String sourceFilePath) {
     try (InputStream sourceStream = new FileInputStream(sourceFilePath);
-         OutputStream destinationStream = new FileOutputStream("src/main/resources/" + sourceFilePath)) {
+         OutputStream destinationStream = new FileOutputStream("src/main/resources/" + Paths.get(sourceFilePath).getFileName())) {
 
       byte[] buffer = new byte[4096];
       int bytesRead;
